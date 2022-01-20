13800fd32eec092bfa5eca06a7888a8d154fd413
makoto-kubodera <makoto-kubodera@linked-ideal.com>
2022-01-21 04:16:09 +0900

    https://github.com/toposoid/toposoid-deduction-unit-synonym-match-web/issues/21

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /app/toposoid-deduction-unit-synonym-match-web/target/universal
-toposoid-deduction-unit-synonym-match-web-0.2/bin/toposoid-deduction-unit-synonym-match-web
+toposoid-deduction-unit-synonym-match-web-0.3-SNAPSHOT/bin/toposoid-deduction-unit-synonym-match-web
